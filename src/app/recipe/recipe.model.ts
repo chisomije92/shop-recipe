6be93177d3abd9d9530f565ada35f8a1db02ac46
Chisom Ijeomah <chisomije92@gmail.com>
2022-11-03 09:09:54 +0100

    add ingredients to recipe

--- src/app/recipe/recipe.model.ts
+++ src/app/recipe/recipe.model.ts
@@ -1,7 +1,10 @@
+import { IngredientsModel } from '../shared/ingredients.model';
+
 export class RecipeModel {
   constructor(
     public name: string,
     public description: string,
-    public imagePath: string
+    public imagePath: string,
+    public ingredients: IngredientsModel[]
   ) {}
 }
--- src/app/recipe/recipe.service.ts
+++ src/app/recipe/recipe.service.ts
@@ -1,18 +1,24 @@
 import { EventEmitter } from '@angular/core';
+import { IngredientsModel } from '../shared/ingredients.model';
 import { RecipeModel } from './recipe.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<RecipeModel>();
   private recipes: RecipeModel[] = [
     new RecipeModel(
-      'A test Recipe',
-      'This is a test',
-      'https://static.onecms.io/wp-content/uploads/sites/44/2022/03/16/sheet-pan-sweet-potato-fajitas.jpg'
+      'Tasty Schnitzel',
+      'Awesome and delicious',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Hamburger_%28black_bg%29.jpg/800px-Hamburger_%28black_bg%29.jpg',
+      [
+        new IngredientsModel('Meat', 1),
+        new IngredientsModel('French Fries', 20),
+      ]
     ),
     new RecipeModel(
-      'Another test Recipe',
-      'This is another test',
-      'https://static.onecms.io/wp-content/uploads/sites/44/2022/03/16/sheet-pan-sweet-potato-fajitas.jpg'
+      'Big Fat Burger',
+      'Juicy, tasty and yummy',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Breitenlesau_Krug_Br%C3%A4u_Schnitzel.JPG/1200px-Breitenlesau_Krug_Br%C3%A4u_Schnitzel.JPG',
+      [new IngredientsModel('Buns', 2), new IngredientsModel('Meat', 2)]
     ),
   ];
 
