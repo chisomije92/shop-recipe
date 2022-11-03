5e36de3d848843dcf1d6516a3a5b903bfd1665ee
Chisom Ijeomah <chisomije92@gmail.com>
2022-11-03 08:27:59 +0100

    manage recipe in recipes service

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { RecipeModel } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,22 +9,25 @@ import { RecipeModel } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<RecipeModel>();
-  recipes: RecipeModel[] = [
-    new RecipeModel(
-      'A test Recipe',
-      'This is a test',
-      'https://static.onecms.io/wp-content/uploads/sites/44/2022/03/16/sheet-pan-sweet-potato-fajitas.jpg'
-    ),
-    new RecipeModel(
-      'Another test Recipe',
-      'This is another test',
-      'https://static.onecms.io/wp-content/uploads/sites/44/2022/03/16/sheet-pan-sweet-potato-fajitas.jpg'
-    ),
-  ];
+  recipes?: RecipeModel[];
+  //recipes: RecipeModel[] = [
+  //  new RecipeModel(
+  //    'A test Recipe',
+  //    'This is a test',
+  //    'https://static.onecms.io/wp-content/uploads/sites/44/2022/03/16/sheet-pan-sweet-potato-fajitas.jpg'
+  //  ),
+  //  new RecipeModel(
+  //    'Another test Recipe',
+  //    'This is another test',
+  //    'https://static.onecms.io/wp-content/uploads/sites/44/2022/03/16/sheet-pan-sweet-potato-fajitas.jpg'
+  //  ),
+  //];
 
-  constructor() {}
+  constructor(private recipeService: RecipeService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipe();
+  }
 
   onRecipeSelected(recipe: RecipeModel) {
     this.recipeWasSelected.emit(recipe);
