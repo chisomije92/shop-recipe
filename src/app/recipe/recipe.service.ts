9ade6475575202226f7d7bd04d2e3d494f489f4c
Chisom Ijeomah <chisomije92@gmail.com>
2022-11-03 08:24:33 +0100

    create service files

---
+++ src/app/recipe/recipe.service.ts
@@ -0,0 +1,20 @@
+import { RecipeModel } from './recipe.model';
+
+export class RecipeService {
+  private recipes: RecipeModel[] = [
+    new RecipeModel(
+      'A test Recipe',
+      'This is a test',
+      'https://static.onecms.io/wp-content/uploads/sites/44/2022/03/16/sheet-pan-sweet-potato-fajitas.jpg'
+    ),
+    new RecipeModel(
+      'Another test Recipe',
+      'This is another test',
+      'https://static.onecms.io/wp-content/uploads/sites/44/2022/03/16/sheet-pan-sweet-potato-fajitas.jpg'
+    ),
+  ];
+
+  getRecipe() {
+    return this.recipes.slice();
+  }
+}
