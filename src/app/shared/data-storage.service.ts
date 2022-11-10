e5be00a09827f08ebdef811dabe82c6e5e655cf1
Chisom Ijeomah <chisomije92@gmail.com>
2022-11-10 13:20:59 +0100

    transform response data

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { RecipeService } from './../recipe/recipe.service';
 import { RecipeModel } from './../recipe/recipe.model';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -22,6 +23,19 @@ export class DataStorageService {
       .get<RecipeModel[]>(
         'https://ng-shop-recipe-90217-default-rtdb.firebaseio.com/recipes.json'
       )
-      .subscribe((recipe) => this.recipeService.setRecipe(recipe));
+      .pipe(
+        map((recipes) => {
+          return recipes.map((recipe) => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : [],
+            };
+          });
+        })
+      )
+      .subscribe((recipes) => {
+        console.log(recipes);
+        return this.recipeService.setRecipe(recipes);
+      });
   }
 }
