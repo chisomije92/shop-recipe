0739c866967cfb51466ab939b6aae0db361c9c94
Chisom Ijeomah <chisomije92@gmail.com>
2022-11-10 12:36:19 +0100

    implement storing recipes in firebase

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,3 +1,5 @@
+import { RecipeService } from './../recipe/recipe.service';
+import { RecipeModel } from './../recipe/recipe.model';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
@@ -5,5 +7,16 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root',
 })
 export class DataStorageService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storedRecipe() {
+    const recipes = this.recipeService.getRecipe();
+    return this.http.put(
+      'https://ng-shop-recipe-90217-default-rtdb.firebaseio.com/recipes.json',
+      recipes
+    );
+    //.subscribe((response) => {
+    //  console.log(response);
+    //});
+  }
 }
